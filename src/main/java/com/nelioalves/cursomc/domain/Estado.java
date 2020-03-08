57138de7aec5fc4a8e2ff88b3e9f694fc2ffa1b0
Geovane Junior <geovane.gjunior@gmail.com>
2020-03-08 00:11:17 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/nelioalves/cursomc/domain/Estado.java
+++ src/main/java/com/nelioalves/cursomc/domain/Estado.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Estado implements Serializable {
 
@@ -27,6 +29,7 @@ public class Estado implements Serializable {
 	@Column(length = 2)
 	private String uf;
 	
+	@JsonBackReference
 	@OneToMany(mappedBy = "estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
