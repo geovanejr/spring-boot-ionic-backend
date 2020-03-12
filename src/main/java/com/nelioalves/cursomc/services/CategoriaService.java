c0408d1e44e7a2c1aa47cbfc497abed3b4b01167
Geovane Junior <geovane.gjunior@gmail.com>
2020-03-12 20:38:30 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -32,7 +35,6 @@ public class CategoriaService {
 	
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
-//		System.out.println("Categoria Service: " + obj.toString());
 		return repo.save(obj);
 	}
 
@@ -44,5 +46,11 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findByPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
