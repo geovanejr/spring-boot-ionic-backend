a73d1993a707bb65021b86676def8e804f8ce5c3
Geovane Junior <geovane.gjunior@gmail.com>
2020-03-11 21:34:01 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,5 +39,10 @@ public class CategoriaService {
 	public void delete(Integer id) {
 		find(id);
 		repo.deleteById(id);
+	}
+	
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
 	}
 }
